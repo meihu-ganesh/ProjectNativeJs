c7f154666417cab1766a1e96e45a20a7a16ed77e
CSNR Ganesh <100460284+meihu-ganesh@users.noreply.github.com>
2023-05-10 17:41:15 +0530

    Native Env Setup

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 40}}>Hello World!!</Text>
+    </View>
+  );
+};
+
+export default App;
