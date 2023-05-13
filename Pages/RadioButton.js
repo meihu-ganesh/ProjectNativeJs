8dcad135d6fab8030ff8e6829429b509e2ffb5ac
CSNR Ganesh <100460284+meihu-ganesh@users.noreply.github.com>
2023-05-13 20:36:56 +0530

    dynamic Radio

--- Pages/RadioButton.js
+++ Pages/RadioButton.js
@@ -3,13 +3,36 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const RadioButton = () => {
 
+    const skills = [
+        {
+            id: 1,
+            name: 'Java',
+        },
+        {
+            id: 2,
+            name: 'Python',
+        },
+        {
+            id: 3,
+            name: 'JavaScript',
+        },
+        {
+            id: 4,
+            name: 'PHP',
+        },
+        {
+            id: 5,
+            name: 'SQL',
+        },
+    ]
+
     const [selectedRadio1, setSelectedRadio1] = useState(false);
     const [selectedRadio2, setSelectedRadio2] = useState(false);
     const [selectedRadio, setSelectedRadio] = useState(false);
 
     return (
         <View style={styles.main}>
-            <View>
+            {/* <View>
                 <Text style={styles.question}>Multiple Select</Text>
                 <TouchableOpacity onPress={()=> setSelectedRadio1(!selectedRadio1)}>
                     <View style={styles.radioWrapper}>
@@ -31,29 +54,26 @@ const RadioButton = () => {
                         <Text style={styles.radioText}>2nd option</Text>
                     </View>
                 </TouchableOpacity>
-            </View>
+            </View> */}
             <View>
                 <Text style={styles.question}>Single Select</Text>
-                <TouchableOpacity onPress={()=> setSelectedRadio(!selectedRadio)}>
+                {
+                    // skills.map((item)=> 
+                    skills.map((item, index)=> 
+                    <TouchableOpacity
+                        key={index}
+                        // key={item.id}
+                        onPress={()=> setSelectedRadio(item.id)}>
                     <View style={styles.radioWrapper}>
                         <View style={styles.radio}>
                             {
-                                selectedRadio ? <View style={styles.radioBg}></View> : null
+                                selectedRadio == item.id ? <View style={styles.radioBg}></View> : null
                             }
                         </View>
-                        <Text style={styles.radioText}> 1st Option</Text>
+                        <Text style={styles.radioText}>{item.name}</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=> setSelectedRadio(!selectedRadio)}>
-                    <View style={styles.radioWrapper}>
-                        <View style={styles.radio}>
-                            {
-                                selectedRadio ? null : <View style={styles.radioBg}></View>
-                            }
-                        </View>
-                        <Text style={styles.radioText}>2nd option</Text>
-                    </View>
-                </TouchableOpacity>
+                )}
             </View>
         </View>
     );
