3fba6efb5d28c8a8152294f28296cca342e3d0c5
CSNR Ganesh <100460284+meihu-ganesh@users.noreply.github.com>
2023-05-14 22:20:45 +0530

    Json server API Calling

--- components/APIPlaying.js
+++ components/APIPlaying.js
@@ -4,40 +4,37 @@ import ExStyles from "../style";
 
 const APIPlaying = () => {
 
-    const [data,setData] = useState([]);
+    const [data, setData] = useState([]);
 
     const getAPIData = async () => {
-        // api call
-        // // console.warn("Hello");
-        const url = 'https://jsonplaceholder.typicode.com/posts';
-        let result = await fetch(url);
-        result = await result.json();
-        // // console.warn(result);
-        setData(result);
-    };
+        // console.warn("Function Calleed");
+        const url = "http://10.0.2.2:3000/users";
+        let results = await fetch(url);
+        results = await results.json();
+        // console.warn(results);
+        setData(results);
+    }
 
     useEffect(()=> {
         getAPIData();
     }, []);
 
     return (
-        <ScrollView>
-            <Text style={ExStyles.heading}>List with API Call</Text>
+        <View>
+            <Text style={ExStyles.heading}>Call JSON Server API</Text>
             {
-                data.length ? 
-                <FlatList 
-                    data={data}
-                    renderItem={({item})=> 
-                        <View style={{borderBottomColor: "#333", borderBottomWidth: 1,padding:12}}>
-                            <Text style={{fontSize: 20}}>Id: {item.id}</Text>
-                            <Text style={{fontSize: 20}}>Title: {item.title}</Text>
-                            <Text style={{fontSize: 20}}>Body: {item.body}</Text>
-                        </View>
-                    }
-                />
-                    : null
+                data.length ?
+                data.map((item)=>
+                    <View style={{borderWidth: 1, borderColor: "#333"}}>
+                        <Text style={{fontSize: 20, }}>{item.id}</Text>
+                        <Text style={{fontSize: 20, }}>{item.name}</Text>
+                        <Text style={{fontSize: 20, }}>{item.age}</Text>
+                        <Text style={{fontSize: 20, }}>{item.email}</Text>
+                    </View>
+                )
+                : null
             }
-        </ScrollView>
+        </View>
     );
 };
 
